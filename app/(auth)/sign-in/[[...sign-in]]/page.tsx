843a467f167fdfa67a157a689153e2ef450011cb
WaliaJatin42 <waliajatin42@gmail.com>
2024-09-01 00:57:07 +0530

    Created full sign in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,10 @@
 import { SignIn, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
 import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Page() {
     return (
-        <div className='min-h-screen grid grid-cols-1 lg:grid'>
+        <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
             <div className='h-full lg:flex flex-col items-center justify-center px-4'>
                 <div className='text-center space-y-4 pt-16'>
                     <h1 className='font-bold text-3xl text-[#2e2a47]'>
@@ -21,6 +22,9 @@ export default function Page() {
                         <Loader2 className='animate-spin text-muted-foreground' />
                     </ClerkLoading>
                 </div>
+            </div>
+            <div className='h-full bg-blue-600 hidden lg:flex items-center justify-center'>
+                <Image src="./logo.svg" alt='logo' height={100} width={100} />
             </div>
         </div>
     )
