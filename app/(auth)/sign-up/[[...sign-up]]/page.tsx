160dbb137133f97842dab3a08cd7382f6f7b65a5
WaliaJatin42 <waliajatin42@gmail.com>
2024-09-01 12:25:59 +0530

    changed the signin and signup page UI

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     return (
         <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
             <div className='h-full lg:flex flex-col items-center justify-center px-4'>
-                <div className='text-center space-y-4 pt-16'>
+                <div className='text-center space-y-2 pt-10'>
                     <h1 className='font-bold text-3xl text-[#2e2a47]'>
                         Welcome Back!
                     </h1>
@@ -14,7 +14,7 @@ export default function Page() {
                         Login or create account to get back to your dashboard!
                     </p>
                 </div>
-                <div className='flex items-center justify-center mt-8'>
+                <div className='flex items-center justify-center mt-6'>
                     <ClerkLoaded>
                         <SignUp path='/sign-up' />
                     </ClerkLoaded>
